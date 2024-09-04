3c3eea9f25b6f3390a27cc746606ff3f75956e77
quekhoait <166801979+quekhoait@users.noreply.github.com>
2024-09-04 18:59:10 +0700

    Update news.js

--- js/news.js
+++ js/news.js
@@ -4,7 +4,14 @@ let focusItem = document.querySelectorAll('.focus-item');
 function focusMove() {
     let index = 0;
     setInterval(function () {
-        index+=3; 
+        if(window.innerWidth < 750){
+            index+=1;
+        }else if(window.innerWidth > 750 && window.innerWidth<880){
+            index+=2;
+        }else{
+            index+=3; 
+
+        }
         for (let i = 0; i < focusItem.length; i++) { //Duyệt gắn thuộc tính
             focusItem[i].style.transition = 'transform 0.5s ease';
             focusItem[i].style.transform = `translateX(${-100 * index}%)`;
